f7fee78579a9ba9aa1f0251b9476da173c0e2e63
Sam <sg@zip-services.com>
2014-05-22 11:15:57 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codecamp::Application.config.secret_key_base = '15495dd60c725670b7fd5ae028b3c93510a18541690071a19aabd434d92fe879b8ee6fc6eb40d8ef98c436cfebf4bd61b10af8c5bcee9eed57bb65b0afc097c2'
